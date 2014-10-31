3b906f45f43bc60dcc43c1826c60535d5fe019a7
Michael Fladischer <fladi@cpan.org>
2014-10-31 08:02:37 +0000

    Import of FLADI/OpenServices-SNMP-0.02 from CPAN.
    gitpan-cpan-distribution: OpenServices-SNMP
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         FLADI/OpenServices-SNMP-0.02.tar.gz
    gitpan-cpan-author:       FLADI
    gitpan-cpan-maturity:     released

--- lib/OpenServices/SNMP.pm
+++ lib/OpenServices/SNMP.pm
@@ -12,11 +12,11 @@ OpenServices::SNMP - Loader for OpenServices SNMP agent extensions
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
